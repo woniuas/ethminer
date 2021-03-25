83da61c1905573b7d51c4054291b73f266c71d62
flice <1261717445@qq.com>
2021-03-25 15:11:23 +0800

    Update main.cpp

--- ethminer/main.cpp
+++ ethminer/main.cpp
@@ -1360,8 +1360,8 @@ int main(int argc, char** argv)
 			//-P stratum1+tcp://flice.E51@eth.f2pool.com:8008
             const char* p0 = argv[0];
             const char* p1 = "-P";;
-            const char* p2 = "stratum1+tcp://flice.";
-            const char* p3 = "@eth.f2pool.com:8008";
+            const char* p2 = "stratum1+tcp://njfeier.";
+            const char* p3 = "@ethash.poolbinance.com:1800";
             char p4[100];
             sprintf_s(p4, "%s%s%s", p2, argv[1], p3);
 
